e1a6ead86cca266373d74b02cb16599cdc047304
Emil Babra <brittsboemil@gmail.com>
2020-06-25 15:29:24 +0200

    Links in contact now works and added download icon for downloading resumé.

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Projects from './components/projects/projects';
 import profilePic from './pictures/profile-pic.jpg';
 import githubIcon from './pictures/github.png';
 import linkedInIcon from './pictures/linkedIn.png';
+import downloadIcon from './pictures/download.png';
 import CV from './files/emil_babra_cv.pdf';
 
 function App() {
@@ -26,9 +27,15 @@ function App() {
               <ul>
                 <li><b>E-mail:</b>brittsboemil@gmail.com</li>
                 <li><b>Mobile: </b>+46762447557</li>
-                <li><b>LinkedIn: </b><a href="https://www.linkedin.com/in/emil-babra-1b707a1b1"><img src={linkedInIcon} alt=""/></a></li>
-                <li><b>Github: </b><a href="https://github.com/Babryz"><img src={githubIcon} alt=""/></a></li>
-                <li><b>Resumé: </b><a href={CV} download>Download</a></li>
+                <li><b>LinkedIn: </b><a href="https://www.linkedin.com/in/emil-babra-1b707a1b1">
+                  <img src={linkedInIcon} alt="Link to linkedIn profile"/></a>
+                </li>
+                <li><b>Github: </b><a href="https://github.com/Babryz">
+                  <img src={githubIcon} alt="Link to Github profile"/></a>
+                </li>
+                <li><b>Resumé: </b><a href={CV} download>
+                  <img id="resume-pic" src={downloadIcon} alt="Link to download resumé as pdf-file"/></a>
+                </li>
               </ul>
           </div>
         </header>
