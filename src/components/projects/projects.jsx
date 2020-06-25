b6c2cc9850a832b155ce4d186c45dcb710bceebd
Emil Babra <brittsboemil@gmail.com>
2020-06-25 13:21:57 +0200

    Add projects part of page and added 3 projects to it

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -3,13 +3,31 @@ import './projects.css';
 
 const Projects = () => {
     return (
-        <div className="container" id="projects">
-            <h3>Projects</h3>
-            <div>
+        <div className="container projects">
+            <h3>What have I built?</h3>
+            <div id="projects">
                 <div className="project" id="weather-app">
                     <h4>React Weather App</h4>
-                    <div>
-                        <button><a href="https://github.com/chas-academy/u09-react-weather-app">Code</a></button>
+                    <div id="background">
+                        <div>
+                            <a href="https://github.com/chas-academy/u09-react-weather-app">Code</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="project" id="movie-app">
+                    <h4>Angular Movie App</h4>
+                    <div id="background">
+                        <div>
+                            <a href="https://github.com/chas-academy/u07-angular-movie-app">Code</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="project" id="hangman-game">
+                    <h4>Hangman Game</h4>
+                    <div id="background">
+                        <div>
+                            <a href="https://github.com/Babryz/hangmang">Code</a>
+                        </div>
                     </div>
                 </div>
             </div>
